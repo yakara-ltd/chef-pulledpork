ed612d788d7d74f03f672fa5ab0a47c4b8d1b9f3
Neil Duff-Howie <neil.howie@yahoo.co.uk>
2016-11-11 11:32:42 +0000

    Add proxy workaround to crontab

--- recipes/service.rb
+++ recipes/service.rb
@@ -3,5 +3,5 @@
 cron 'pulledpork' do
   hour '12'
   minute '0'
-  command "/usr/local/bin/pulledpork.pl -c #{node['pulledpork']['pp_config_path']} -l && service #{node['pulledpork']['snort_svc_name']} restart"
+  command "/usr/local/bin/pulledpork.pl -c #{node['pulledpork']['pp_config_path']} -l -W && service #{node['pulledpork']['snort_svc_name']} restart"
 end
